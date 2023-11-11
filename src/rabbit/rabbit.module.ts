c3c39e618162cc2d0f062afdc4a631ff63056d0d
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-12 00:26:47 +0100

    chore: rabbit server and client

--- src/rabbit/rabbit.module.ts
+++ src/rabbit/rabbit.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { RabbitController } from './rabbit.controller';
+import { RabbitMQClient } from './rabbit.client';
 
 @Module({
   controllers: [RabbitController],
+  providers: [RabbitMQClient, RabbitMQClient],
+  exports: [RabbitMQClient],
 })
 export class RabbitModule {}
